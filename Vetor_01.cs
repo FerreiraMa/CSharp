9a65fa1db4b17b4206c7087b7ac9618162c57b92
FerreiraMa <ferreirasantos1976@uol.com.br>
2022-11-04 11:15:27 -0300

    Exemplo de vetor em c# 01

--- Vetor_01.cs
+++ Vetor_01.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Vetor
 {
@@ -10,7 +7,26 @@ namespace Vetor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teste");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("A média é: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
         }
     }
 }
